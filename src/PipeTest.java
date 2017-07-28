25eb780638b001bd16df43d2235ab50c648a4c84
zhaoshq <523618033@qq.com>
2017-07-28 15:59:49 +0800

    NIO中pipe（管道）测试

---
+++ src/PipeTest.java
@@ -0,0 +1,89 @@
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.Pipe;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ *
+ * java NIO 中的pipe是2个线程之间的单向数据连接。Pipe有一个source通道和一个sink通道。数据会被写到sink通道，从source通道读取。
+ * Created by zhaoshq on 2017/7/28.
+ */
+public class PipeTest {
+
+    public static void pipeMethod(){
+        Pipe pipe = null;
+        ExecutorService exec = Executors.newFixedThreadPool(2);
+
+        try {
+            pipe = Pipe.open();
+            final Pipe pipetemp = pipe;
+
+            exec.submit(new Runnable() {
+                @Override
+                public void run()  {
+                    try {
+                        Pipe.SinkChannel sinkChannel = pipetemp.sink(); //向通道内写数据
+                        while (true){
+                            TimeUnit.SECONDS.sleep(1);
+                            String datas = "Pipe Test At Time "+System.currentTimeMillis();
+                            ByteBuffer buffer = ByteBuffer.allocate(1024);
+
+                            buffer.clear();
+                            buffer.put(datas.getBytes());
+
+                            buffer.flip();
+                            while (buffer.hasRemaining()){
+                                sinkChannel.write(buffer);
+                            }
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            });
+
+            exec.submit(new Runnable() {
+                @Override
+                public void run()  {
+                    try {
+                        Pipe.SourceChannel sourceChannel = pipetemp.source(); //从通道内读数据
+                        while (true){
+                            TimeUnit.SECONDS.sleep(3);
+                            ByteBuffer buffer = ByteBuffer.allocate(1024);
+                            //从buffer中写数据
+                            buffer.clear();
+                            int bytesread = sourceChannel.read(buffer);
+                            while (bytesread != -1){
+
+                                //从buffer中读数据
+                                buffer.flip();
+                                while (buffer.hasRemaining()){
+                                    System.out.print((char) buffer.get());
+                                }
+                                System.out.println();
+                                //从buffer中写数据
+                                buffer.clear();
+                                bytesread = sourceChannel.read(buffer);
+                                TimeUnit.SECONDS.sleep(3);
+                            }
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            });
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        pipeMethod();
+    }
+}
